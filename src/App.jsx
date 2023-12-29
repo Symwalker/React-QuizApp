431b3c88297e57c8b2eac37863051e2c13bb9865
Symwalker <pmed27397@gmail.com>
2023-12-29 17:59:39 +0500

    final stages

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,5 @@
-// import React, { useEffect, useState } from 'react'
 
-// const App = () => {
-//     const [questions, setQuestions] = useState([])
-//     const [currentIndex, setCurrentIndex] = useState(0)
-//     const [answers, setAnswers] = useState([])
-//     console.log(answers);
-//     const getQuestionFromApi = () => {
-//         fetch('https://the-trivia-api.com/v2/questions')
-//             .then((res) => {
-//                 return res.json()
-//             })
-//             .then((res) => {
-//                 setQuestions(res)
-               
-//             })
 
-//     }
-//     const nextQuestion = () => {
-//         setCurrentIndex(currentIndex + 1)
-//     }
-//     const reStart = () => {
-//         setCurrentIndex(0)
-//     }
-
-//     useEffect(() => {
-//         getQuestionFromApi()
-        
-//     }, [])
-//     if (!questions.length) {
-//         return <div>
-//             loading
-//         </div>
-//     }
-//     return (
-//         <div>
-//             <div>
-//                 <h2>{questions[currentIndex].question.text}</h2>
-//             </div>
-//             {/* answers */}
-//             <div>
-//                 <ul>
-//                     {
-//                         answers.map((item, i) => (
-//                             <li key={i}>{item}</li>
-//                         ))
-//                     }
-//                 </ul>
-//             </div>
-//             {
-//                 currentIndex === questions.length - 1 ? (
-//                     <button className='bg-black text-white p-2 rounded-md' onClick={reStart}>Re-Start</button>
-//                 ) : (
-//                     <button className='bg-black text-white p-2 rounded-md' onClick={nextQuestion}>nextQueston</button>
-//                 )
-//             }
-//         </div>
-//     )
-// }
-
-// export default App
 
 import React, { useEffect, useState } from 'react';
 
@@ -66,7 +7,19 @@ const App = () => {
     const [questions, setQuestions] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
     const [answers, setAnswers] = useState([]);
+    const [ShowResult, setShowResult] = useState(false);
+    const [score, setScore] = useState(0);
+    const [slectedAnswer, setSelectedAnswer] = useState(null)
     console.log(answers);
+    console.log(questions)
+    const shuffleArray = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            console.log(j, " ", i);
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
 
     const getQuestionFromApi = () => {
         fetch('https://the-trivia-api.com/v2/questions')
@@ -74,49 +27,86 @@ const App = () => {
             .then((res) => {
                 const inCorrectAnswers = res[currentIndex].incorrectAnswers;
                 const ans = [...inCorrectAnswers, res[currentIndex].correctAnswer];
-                setAnswers(ans);
+                const shuffledAnswers = shuffleArray(ans);
+                setAnswers(shuffledAnswers);
                 setQuestions(res);
+                setSelectedAnswer(null)
             });
     };
 
     const nextQuestion = () => {
+        const corrAnswer = questions[currentIndex].correctAnswer
+        if (slectedAnswer === corrAnswer) {
+            setScore(score + 1)
+        }
         setCurrentIndex(currentIndex + 1);
     };
 
     const reStart = () => {
+        setShowResult(false)
+
         setCurrentIndex(0);
     };
+    const result = () => {
+        setShowResult(true)
+    };
+
+    const handleSelectedAnswer = (i) => {
+        // const answe = questions[i]
+        console.log(i);
+        setSelectedAnswer(i)
+    };
+
 
     useEffect(() => {
         getQuestionFromApi();
-    }, []);
+    }, [currentIndex]); // Include currentIndex as a dependency to trigger the effect when it changes
 
     if (!questions.length) {
         return <div>loading</div>;
     }
 
     return (
-        <div>
-            <div>
-                <h2>{questions[currentIndex].question.text}</h2>
+        <div className='bg-gray-200 h-screen p-10'>
+
+            <div className='mx-auto rounded-lg z-50 bg-white max-w-[550px]  hadow-sm '>
+                {
+                    ShowResult ? (
+                        <div className='p-2'>
+                            <h2 className='text-[22px] '>Your score is <q className='font-semibold'>{score}</q></h2>
+                            <button className='bg-blue-700 text-white font-semibold text-[18px] p-2 rounded-md mt-3' onClick={reStart}>
+                                Restart
+                            </button>
+                        </div>
+                    ) : (
+                        <div className='flex justify-between flex-col gap-10'>
+                            <div>
+                                <h2 className='text-[22px] font-bold p-1'>Q{currentIndex + 1}) {questions[currentIndex].question.text}</h2>
+                            </div>
+                            <ul>
+                                {answers.map((item, i) => (
+                                    // <li key={i}>{item}</li>
+                                    <li className='flex items-center gap-4 p-1'>
+                                        <input className='w-[20px] h-[20px]' type="checkbox" onChange={() => handleSelectedAnswer(item)} checked={slectedAnswer === item} />
+                                        <label className='text-[20px]' >{item}</label>
+                                    </li>
+                                ))}
+                            </ul>
+                            <div className=''>
+                                {currentIndex === questions.length - 1 ? (
+                                    <button className='w-full bg-black text-white p-2 rounded-md' onClick={result}>
+                                        Show Result
+                                    </button>
+                                ) : (
+                                    <button className='w-full bg-blue-800 text-white font-semibold text-[18px] p-2 rounded-md' onClick={nextQuestion}>
+                                        Next Question
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                    )
+                }
             </div>
-            {/* answers */}
-            <div>
-                <ul>
-                    {answers.map((item, i) => (
-                        <li key={i}>{item}</li>
-                    ))}
-                </ul>
-            </div>
-            {currentIndex === questions.length - 1 ? (
-                <button className='bg-black text-white p-2 rounded-md' onClick={reStart}>
-                    Re-Start
-                </button>
-            ) : (
-                <button className='bg-black text-white p-2 rounded-md' onClick={nextQuestion}>
-                    nextQueston
-                </button>
-            )}
         </div>
     );
 };
